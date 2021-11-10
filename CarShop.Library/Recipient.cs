04a34d3fc93c75c9e277f9635d7208dde0fa50d6
HOLDING\102829 <102829@norwegian.com>
2021-11-10 21:04:43 +0200

    refactoring

---
+++ CarShop.Library/Recipient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.Library
+{
+    public class Recipient
+    {
+        public Car Car { get; set; }
+        public string RecipientId { get; set; }
+        public string RecipientName { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
